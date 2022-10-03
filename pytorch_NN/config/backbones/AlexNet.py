2f36362a625e7dd18a3caacfea6bccd698efc74f
Kopion-Shi <Carl3633524461@outlook.com>
2022-10-03 15:25:02 +0800

    backbones

---
+++ pytorch_NN/config/backbones/AlexNet.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+class AlexNet():
+    """
+    The input for Alexnet is a 224*224 RGB image
+    Args:
+         num_classes (int): number of classes for classification.
+            The default value is -1, which uses the backbone as
+            a feature extractor without the top classifier.
+    """
+
+    def __init__(self, num_classes=0):
+        # super(AlexNet, self).__init__()
+        self.num_classes = num_classes
+        self.features = nn.Sequential(
+        # 这里，我们使用一个11*11的更大窗口来捕捉对象。
+        # 同时，步幅为4，以减少输出的高度和宽度。
+        # 另外，输出通道的数目远大于LeNet
+        nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1), nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        # 减小卷积窗口，使用填充为2来使得输入与输出的高和宽一致，且增大输出通道数
+        nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        # 使用三个连续的卷积层和较小的卷积窗口。
+        # 除了最后的卷积层，输出通道的数量进一步增加。
+        # 在前两个卷积层之后，汇聚层不用于减少输入的高度和宽度
+        nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(),
+        nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(),
+        nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        nn.Flatten(),
+        )
+        if self.num_classes > 0:
+            self.classifier = nn.Sequential(
+                nn.Dropout(),
+                nn.Linear(256 * 6 * 6, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace=True),
+                nn.Linear(4096, num_classes),
+            )
+
+    def forward(self, x):
+        x = self.features(x)
+        if self.num_classes > 0:
+            x = x.view(x.size(0), 256 * 6 * 6)
+            x = self.classifier(x)
+        return (x,)
+    def feature(self):
+         net = nn.Sequential(
+            # 这里，我们使用一个11*11的更大窗口来捕捉对象。
+            # 同时，步幅为4，以减少输出的高度和宽度。
+            # 另外，输出通道的数目远大于LeNet
+            nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            # 减小卷积窗口，使用填充为2来使得输入与输出的高和宽一致，且增大输出通道数
+            nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            # 使用三个连续的卷积层和较小的卷积窗口。
+            # 除了最后的卷积层，输出通道的数量进一步增加。
+            # 在前两个卷积层之后，汇聚层不用于减少输入的高度和宽度
+            nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Flatten(),
+            # 这里，全连接层的输出数量是LeNet中的好几倍。使用dropout层来减轻过拟合
+            nn.Linear(6400, 4096), nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096), nn.ReLU(),
+            nn.Dropout(p=0.5),
+            # 最后是输出层。由于这里使用Fashion-MNIST，所以用类别数为10，而非论文中的1000
+            nn.Linear(4096, 10))
+         return net
+    
